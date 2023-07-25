5420559f5c6a464db1fe550ed881e85cee67ff89
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-07-25 22:09:56 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import random
+from misskey import Misskey
+import os
+
+f = open("answer.txt", "r")
+answer = f.read()
+f.close()
+
+x = random.randint(1, 9999)
+y = random.randint(1, 9999)
+
+h = open("answer.txt", "w")
+h.write(str(y))
+h.close()
+
+# Misskey
+misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
+misskey_token = os.environ.get("MISSKEY_TOKEN")
+api = Misskey(misskey_address)
+api.token = misskey_token
+
+match random.randint(1, 4):
+    case 1:
+        post_text = "𝕏にあてはまる数字を求めよ\n"+str(x) + " × 𝕏 =" + str(x * y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
+        print(post_text)
+        api.notes_create(text=post_text)
+       
+    case 2:
+        post_text = "𝕏にあてはまる数字を求めよ\n"+str(x) + " + 𝕏 =" + str(x + y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
+        print(post_text)
+        api.notes_create(text=post_text)
+
+    case 3:
+        post_text = "𝕏にあてはまる数字を求めよ\n"+str(x) + " - 𝕏 =" + str(x - y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
+        print(post_text)
+        api.notes_create(text=post_text)
+
+
+    case 4:
+        post_text = "𝕏にあてはまる数字を求めよ\n"+str(x) + " ÷ 𝕏 =" + str(x / y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
+        print(post_text)
+        api.notes_create(text=post_text)
