698dc52c9e1dcb7720e7f882cd46042885d3235d
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-07-25 23:01:06 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -31,14 +31,14 @@ match random.randint(1, 4):
         api.notes_create(text=post_text)
 
     case 3:
-        post_text = "前回の答えは\"𝕏 = "+ answer +"\" でした!　では次の問題です\n\n𝕏にあてはまる数字を求めよ\n"+str(x) + " - 𝕏 = " + str(x - y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
+        post_text = "前回の答えは\":unicode_1d54f_bg_black: = "+ answer +"\" でした!　では次の問題です\n\n:unicode_1d54f_bg_black:にあてはまる数字を求めよ\n"+str(x) + " - :unicode_1d54f_bg_black: = " + str(x - y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
         print(post_text)
         api.notes_create(text=post_text)
 
 
     case 4:
-        post_text = "前回の答えは\"𝕏 = "+ answer +"\" でした！　では次の問題です\n\n𝕏にあてはまる数字を求めよ\n"+str(x) + " ÷ 𝕏 = " + str(x / y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
+        post_text = "前回の答えは\":unicode_1d54f_bg_black: = "+ answer +"\" でした！　では次の問題です\n\n:unicode_1d54f_bg_black:にあてはまる数字を求めよ\n"+str(x) + " ÷ :unicode_1d54f_bg_black: = " + str(x / y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
         print(post_text)
         api.notes_create(text=post_text)
 
-print("𝕏 = "+str(y))
+print(":unicode_1d54f_bg_black: = "+str(y))
