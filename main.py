7a1b6ad7859ac181c1fe0926a7f0d1067a1db8e5
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-10-23 12:09:38 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import random
 from misskey import Misskey
 import os
 
-f = open("answer.txt", "r")
-answer = f.read()
+f = open("question.txt", "r", encoding='utf-8')
+old_file = f.read()
+old = old_file.split("\n")
 f.close()
 
 x = random.randint(1, 9999)
 y = random.randint(1, 9999)
-
-h = open("answer.txt", "w")
-h.write(str(y))
-h.close()
 
 # Misskey
 misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
@@ -20,25 +17,40 @@ api = Misskey(misskey_address)
 api.token = misskey_token
 
 match random.randint(1, 4):
-    case 1:
-        post_text = "前回の答えは\":unicode_1d54f_bg_black: = "+ answer +"\" でした！　では次の問題です\n\n:unicode_1d54f_bg_black:にあてはまる数字を求めよ\n"+str(x) + " × :unicode_1d54f_bg_black: = " + str(x * y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
-        print(post_text)
-        api.notes_create(text=post_text)
-       
-    case 2:
-        post_text = "前回の答えは\":unicode_1d54f_bg_black: = "+ answer +"\" でした！　では次の問題です\n\n:unicode_1d54f_bg_black:にあてはまる数字を求めよ\n"+str(x) + " + :unicode_1d54f_bg_black: = " + str(x + y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
-        print(post_text)
-        api.notes_create(text=post_text)
+        case 1:
+            post_text = "前回の答えは\":unicode_1d54f_bg_black: = "+ old[1] +"\" でした！　では次の問題です\n\n:unicode_1d54f_bg_black:にあてはまる数字を求めよ\n"+str(x) + " × :unicode_1d54f_bg_black: = " + str(x * y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
+            h = open("question.txt", "w", encoding='utf-8')
+            h.write(str(x)+"\n"+str(y)+'\n'+'×'+'\n'+str(x * y)+"\n"+old[1])
+            h.close()
+            print(post_text)
+            api.notes_create(text=post_text)
+        
+        case 2:
+            post_text = "前回の答えは\":unicode_1d54f_bg_black: = "+ old[1] +"\" でした！　では次の問題です\n\n:unicode_1d54f_bg_black:にあてはまる数字を求めよ\n"+str(x) + " + :unicode_1d54f_bg_black: = " + str(x + y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
+            h = open("question.txt", "w", encoding='utf-8')
+            h.write(str(x)+"\n"+str(y)+'\n'+'+'+'\n'+str(x + y)+"\n"+old[1])
+            h.close()
+            print(post_text)
+            api.notes_create(text=post_text)
 
-    case 3:
-        post_text = "前回の答えは\":unicode_1d54f_bg_black: = "+ answer +"\" でした!　では次の問題です\n\n:unicode_1d54f_bg_black:にあてはまる数字を求めよ\n"+str(x) + " - :unicode_1d54f_bg_black: = " + str(x - y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
-        print(post_text)
-        api.notes_create(text=post_text)
+        case 3:
+            post_text = "前回の答えは\":unicode_1d54f_bg_black: = "+ old[1] +"\" でした!　では次の問題です\n\n:unicode_1d54f_bg_black:にあてはまる数字を求めよ\n"+str(x) + " - :unicode_1d54f_bg_black: = " + str(x - y) +"\n\n回答は約1時間後に次の問題と一緒に開示されます"
+            h = open("question.txt", "w", encoding='utf-8')
+            h.write(str(x)+"\n"+str(y)+'\n'+'-'+'\n'+str(x - y)+"\n"+old[1])
+            h.close()
+            print(post_text)
+            api.notes_create(text=post_text)
 
 
-    case 4:
-        post_text = "前回の答えは\":unicode_1d54f_bg_black: = "+ answer +"\" でした！　では次の問題です\n\n:unicode_1d54f_bg_black:にあてはまる数字を求めよ\n"+str(x) + " ÷ :unicode_1d54f_bg_black: = " + str(x / y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
-        print(post_text)
-        api.notes_create(text=post_text)
+        case 4:
+            post_text = "前回の答えは\":unicode_1d54f_bg_black: = "+ old[1] +"\" でした！　では次の問題です\n\n:unicode_1d54f_bg_black:にあてはまる数字を求めよ\n"+str(x) + " ÷ :unicode_1d54f_bg_black: = " + str(x / y)+"\n\n回答は約1時間後に次の問題と一緒に開示されます"
+            h = open("question.txt", "w", encoding='utf-8')
+            h.write(str(x)+"\n"+str(y)+'\n'+'÷'+'\n'+str(x / y)+"\n"+old[1])
+            h.close()
+            print(post_text)
+            api.notes_create(text=post_text)
 
-print("X = "+str(y))
+print("--書き込み--")
+print("X = "+str(x))
+print("y = "+str(y))
+print("answer ="+str(x - y))
